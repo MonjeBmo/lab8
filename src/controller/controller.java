9bc93877e95df3c19bcc2fd599c59a17d151f04f
Victor Monje <monjev086@gmail.com>
2023-09-21 21:47:29 -0600

    feat:comment func model_info

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -8,20 +8,28 @@ public class controller {
 
 
     public DefaultTableModel model_info(Object[][] students) {
+        // Crear un nuevo modelo de tabla por defecto
         DefaultTableModel model = new DefaultTableModel();
 
+        // Establecer los encabezados de las columnas de la tabla
         model.setColumnIdentifiers(new String[]{"Nombre Completo", "Email", "Nota 1", "Nota 2", "Nota 3", "Nota 4", "Promedio"});
 
+        // Iterar a través de la matriz de estudiantes
         for (Object[] student : students) {
-
+            // Obtener las notas del estudiante (asumiendo que están en el índice 2 del arreglo de objetos)
             int[] notes = (int[]) student[2];
+
+            // Calcular el promedio de las notas
             double pro = 0;
             for (int note : notes) {
                 pro += note;
             }
 
+            // Agregar una fila al modelo de tabla con la información del estudiante y su promedio
             model.addRow(new Object[]{student[0], student[1], notes[0], notes[1], notes[2], notes[3], (pro / notes.length)});
         }
+
+        // Devolver el modelo de tabla con los datos de los estudiantes y sus promedios
         return model;
     }
 
