fcce16e6ea3070a20962417e8f4790f91e18aa97
Victor Monje <monjev086@gmail.com>
2023-09-21 22:06:27 -0600

    feat: add index on table

--- src/controller/controller.java
+++ src/controller/controller.java
@@ -12,9 +12,10 @@ public class controller {
         DefaultTableModel model = new DefaultTableModel();
 
         // Establecer los encabezados de las columnas de la tabla
-        model.setColumnIdentifiers(new String[]{"Nombre Completo", "Email", "Nota 1", "Nota 2", "Nota 3", "Nota 4", "Promedio"});
+        model.setColumnIdentifiers(new String[]{"No.", "Nombre Completo", "Email", "Nota 1", "Nota 2", "Nota 3", "Nota 4", "Promedio"});
 
         // Iterar a través de la matriz de estudiantes
+        int i = 0;
         for (Object[] student : students) {
             // Obtener las notas del estudiante (asumiendo que están en el índice 2 del arreglo de objetos)
             int[] notes = (int[]) student[2];
@@ -26,7 +27,7 @@ public class controller {
             }
 
             // Agregar una fila al modelo de tabla con la información del estudiante y su promedio
-            model.addRow(new Object[]{student[0], student[1], notes[0], notes[1], notes[2], notes[3], (pro / notes.length)});
+            model.addRow(new Object[]{(i += 1), student[0], student[1], notes[0], notes[1], notes[2], notes[3], (pro / notes.length)});
         }
 
         // Devolver el modelo de tabla con los datos de los estudiantes y sus promedios
